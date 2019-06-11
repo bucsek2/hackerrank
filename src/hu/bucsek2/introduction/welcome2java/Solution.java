44f13a6525a5211b64fb18de04509dcb2cf5edd7
György Bucsek <gyorgy.bucsek@gmail.com>
2019-06-11 11:11:41 +0200

    Add some solutions

---
+++ src/hu/bucsek2/introduction/welcome2java/Solution.java
@@ -0,0 +1,9 @@
+package hu.bucsek2.introduction.welcome2java;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
